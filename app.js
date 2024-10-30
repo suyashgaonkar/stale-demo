e62524e672402f45e63562b7bfc6e4cb63266386
suyashgaonkar <suyashgaonkar@github.com>
2024-10-30 17:40:29 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+const http = require("http");
+const fs = require("fs"); //fileSystem
+
+const PORT_NUMBER = 3200;
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const requestMethod = req.method;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write(
+      '<form action="/message"  method="POST"><input name="message" type="text"/></form>'
+    );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && requestMethod === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+  res.write("<html>");
+  res.write("<body>");
+  res.write("<h1>HI header</h1>");
+  res.write("<div> Hello World </div>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
+}); //createServer creates a server
+
+server.listen(PORT_NUMBER); //listens to incoming requests cotinually
